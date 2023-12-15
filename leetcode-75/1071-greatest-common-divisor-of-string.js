10b66fd955c8a998bb8f0e84fd372e1db2c37011
Pedro Gomez <gomez.pedro@outlook.com>
2023-12-15 11:52:28 -0800

    others solution to greatest common divisor

---
+++ leetcode-75/1071-greatest-common-divisor-of-string.js
@@ -0,0 +1,36 @@
+// For two strings s and t, we say "t divides s" if and only if 
+// s = t + ... + t (i.e., t is concatenated with itself one 
+// or more times).
+
+// Given two strings str1 and str2, return the largest string 
+// x such that x divides both str1 and str2.
+
+
+//Other solution
+/* @param {string} str1
+* @param {string} str2
+* @return {string}
+*/
+var gcdOfStrings = function(str1, str2) {
+   
+   if (str1 + str2 !== str2 + str1) {
+
+       return "";
+   }
+
+   let n = str1.length;
+
+   let k = str2.length;
+
+   let gcds = function (x, y) {
+       
+       if (!y) {
+           return x;
+       }
+       return gcds(y, x % y);
+   }
+
+   let div = gcds (n, k);
+
+   return str1.slice(0, div);
+};
