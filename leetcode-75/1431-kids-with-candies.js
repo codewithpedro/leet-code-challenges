3aa82ca46b650edc854d8287678cbc9f09bf9e93
Pedro Gomez <gomez.pedro@outlook.com>
2023-12-16 12:37:50 -0800

    solutions for kids with candies using array and map

---
+++ leetcode-75/1431-kids-with-candies.js
@@ -0,0 +1,39 @@
+// 1431. Kids With the Greatest Number of Candies
+
+// There are n kids with candies. You are given an integer array 
+// candies, where each candies[i] represents the number of 
+// candies the ith kid has, and an integer extraCandies, 
+// denoting the number of extra candies that you have.
+
+// Return a boolean array result of length n, where result[i] 
+// is true if, after giving the ith kid all the extraCandies, 
+// they will have the greatest number of candies among all the 
+// kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of 
+// candies.
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const greatestNum = Math.max(...candies);
+    const resultArray= [];
+
+    for (const num of candies) {
+        const isHigher = num + extraCandies >= greatestNum;
+        resultArray.push(isHigher);
+    }
+
+    return resultArray;
+};
+
+//Other Solutions
+// Uses map to input the values of true and false
+
+var kidsWithCandiesOne = function(candies, extraCandies) {
+    const mostCandies = Math.max(...candies);
+    return candies.map((candyAmount) => candyAmount + extraCandies >= mostCandies)
+};
